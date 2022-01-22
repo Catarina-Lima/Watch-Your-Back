96adb001e22895fae119c8c32e43cc5e39e8a000
natercia <96394674+natercia@users.noreply.github.com>
2022-01-22 19:48:09 +0000

    i am in misery
    there aint nobody who can comfort me oh yeah why wont you answer me the silence is slowly killing me oh yeah girl you really got me bad you really got me bad
    
    feitos a alavanca que desativa fogo e apanhar a chave / abrir porta

---
+++ MyProject/Assets/Scripts/key.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class key : MonoBehaviour
+{
+    [SerializeField]
+    public Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    GameObject hero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( pickUpAllowed && Input.GetKeyDown(KeyCode.F)){
+            PickUp();
+        }   
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+        if( col.gameObject.name.Equals("hero")){
+            hero = col.gameObject;
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col){
+        if( col.gameObject.name.Equals("hero")){
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    } 
+
+    private void PickUp(){
+        hero.GetComponent<HeroController>().gotkey = true;
+        Destroy(gameObject);
+    }
+}
